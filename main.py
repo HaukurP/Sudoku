53c44495d8e68ceb49a42f35603d3e673f8236e3
Haukur Páll Jónsson <hpj3@hi.is>
2012-08-06 15:43:27 +0000

    added comments in functions and better multiple solutions

--- main.py
+++ main.py
@@ -3,71 +3,95 @@ import sys, sudoku, time
 
 def testboard(board):
     """creates a test board"""
+    # Use: board.testboard()
+    # Before: board is a Board from sudoku
+    # After: board is now configured by the initial input set in this function
     print("test board activated")
-    board.addfinal(9,0,1)
-    board.addfinal(6,0,6)
-    board.addfinal(3,0,8)
-    board.addfinal(9,1,5)
-    board.addfinal(5,1,8)
-    board.addfinal(3,2,0)
-    board.addfinal(2,2,1)
-    board.addfinal(4,2,8)
-    board.addfinal(3,3,1)
-    board.addfinal(2,3,6)
-    board.addfinal(5,3,7)
-    board.addfinal(6,3,8)
-    board.addfinal(9,4,1)
-    board.addfinal(8,4,7)
-    board.addfinal(2,5,0)
-    board.addfinal(8,5,1)
-    board.addfinal(5,5,2)
-    board.addfinal(3,5,7)
-    board.addfinal(3,6,0)
-    board.addfinal(2,6,7)
-    board.addfinal(4,6,8)
-    board.addfinal(8,7,0)
-    board.addfinal(7,7,3)
-    board.addfinal(5,8,0)
-    board.addfinal(9,8,2)
-    board.addfinal(6,8,7)
+    #           board.add_final(number,box,square)
+    board.add_final(9,0,1)
+    board.add_final(6,0,6)
+    board.add_final(3,0,8)
+    board.add_final(9,1,5)
+    board.add_final(5,1,8)
+    board.add_final(3,2,0)
+    board.add_final(2,2,1)
+    board.add_final(4,2,8)
+    board.add_final(3,3,1)
+    board.add_final(2,3,6)
+    board.add_final(5,3,7)
+    board.add_final(6,3,8)
+    board.add_final(9,4,1)
+    board.add_final(8,4,7)
+    board.add_final(2,5,0)
+    board.add_final(8,5,1)
+    board.add_final(5,5,2)
+    board.add_final(3,5,7)
+    board.add_final(3,6,0)
+    board.add_final(2,6,7)
+    board.add_final(4,6,8)
+    board.add_final(8,7,0)
+    board.add_final(7,7,3)
+    board.add_final(5,8,0)
+    board.add_final(9,8,2)
+    board.add_final(6,8,7)
+
 
 def solve(board):
-    board.printboard()
-    trymore = board.trysolving()
+    """attempts to solve a particular board"""
+    # Use: solve(board)
+    # Before: board is a Board
+    # After: board has been solved or new boards have been created and put in a queue
+    try_more = board.try_solving() #try_more checks if some possibilites were removed
 
-    while trymore:
-        trymore = board.trysolving()
+    while try_more:
+        try_more = board.try_solving()
 
-    if board.issolved():
+    if board.is_solved():
         print("Sudoku is solved")
-        board.printboard()
+        solutions.append(board)
+        
         if not stack:
             pass
         else:
             print("Finishing other attempts")
             solve(stack.pop())
     else:
-        depth =+ 1
         print("Sudoku is not solved - attempting multiple solutions")
-        toaddinstack = board.multiplesolutions()
-        for listboard in toaddinstack:
+        to_add_in_stack = board.multiple_solutions()
+        for listboard in to_add_in_stack:
             boards = sudoku.Board()
-            boards.importdata(listboard)
-            stack.append(boards)
+            boards.import_data(listboard)
+            stack.append(boards)    #adding multiple solutions to a queue
                       
     solve(stack.pop())      
     
+def single_solutions(boards):
+    """finds single solutions"""
+    # Use: single_solutions_boards = single_solutions(boards)
+    # Before: boards might contain the same solution often
+    # After: single_solutions_boards is a list only containing single solutions
+    single_solutions = []
+    for board in boards:
+        if board not in single_solutions:
+            single_solutions.append(board)
+    return single_solutions
         
     
 start = time.clock()
 board1 = sudoku.Board()
 testboard(board1)
 stack = [board1]
+solutions = []
 while stack:
     try:
         solve(stack.pop())
     except:
+        pass
         print("No solution")
 
 end = time.clock()-start
-print("End of program - Runtime: {0:.4f} seconds".format(end))
+print("End of program - Time used to solve: {0:.4f} seconds".format(end))
+single_solution = single_solutions(solutions)
+
+for boards in single_solution:
+    boards.print_board()
--- sudoku.py
+++ sudoku.py
@@ -7,23 +7,25 @@ class Board:
     # A False value means that the numeric value(s) are possible solutions
     # The data is set up, looking at sudoku as:
     #     0        1      2
-    # | 0 1 2 | 1 2 3 | ....
+    # | 0 1 2 | 0 1 2 | ....
     # | 3 4 5 | .....
     # | 6 7 8 | ....
     # ______________________
     # |   3   |   4     .
-    # | ..... |         :
+    # | 0 1 2 |  ....   :
     
     def __init__(self):
-        print("creating sudoku")
         self.data = [None]*9
         for i in range(9):
             self.data[i]=[None]*9
             for j in range(9):
                 self.data[i][j] = [1,2,3,4,5,6,7,8,9,False]
 
-    def printboard(self):
+    def print_board(self):
         """prints out the board, badly at start"""
+        # Use: board.print_board()
+        # Before: board is a Board
+        # After: board has been print out
         print("_______________________________________")
         for m in range(3):
             for k in range(3):
@@ -35,34 +37,43 @@ class Board:
                 print()
         print("_______________________________________")
 
-    def importdata(self,bdata):
+    def import_data(self,bdata):
         """imports data from a list to a Board"""
+        # Use: board.import_data(bdata)
+        # Before: board is a Board, bdata is a list that looks like a Board
+        # After: bdata is now a Board
         self.data = copy.deepcopy(bdata)                                             
 
-    def addfinal(self,number,i,j):
+    def add_final(self,number,i,j):
         """adds a number to a square, as an answer"""
+        # Use: board.add_final(number,box,square)
+        # Before: board is a Board, number: 1-9, box: 0-8, square: 0-8
+        # After: number has been added as an answer to the board
         self.data[i][j] = [number, False]
-        self.maketrue()
+        self.make_true()
 
-    def addnumbers(self,numbers,i,j):
+    def add_numbers(self,numbers,i,j):
         """adds a number to a square and checks if it's an answer"""
         #bæta við seinna?
 
-    def checkhoriz(self):
+    def check_horizontal(self):
         """checks what possibilities are not ok, horizontally"""
-        removed = False
+        # Use: board.check_horizontal()
+        # Before: board is a Board
+        # After: if there is a solution in a line that number has been removed as an option through the line
+        removed = False     #removed is a bad solution for checking if something was removed from the board
         for i in range(9):
             for j in range(9):
                 if self.data[i][j][1] == True:
                     m,k = i//3,j//3
-                    if removed:
-                        dontcare = self.removefromline(self.data[i][j][0],m,k)
-                    else:
-                        removed = self.removefromline(self.data[i][j][0],m,k)
+                    removed |= self.remove_from_line(self.data[i][j][0],m,k)
         return removed
 
-    def removefromline(self,number,m,k):
+    def remove_from_line(self,number,m,k):
         """removes a number from a line in the board"""
+        # Use: board.remove_from_line(number,m,k)
+        # Before: board: Board, number: 1-9, m: 0-2, k: 0-2
+        # After: number has been removed from a line denoted by m and k
         removed = False
         for i in range(3):
             for j in range(3):
@@ -72,21 +83,24 @@ class Board:
                         removed = True
         return removed
 
-    def checkvertic(self):
+    def check_vertically(self):
         """checks what possibilities are not ok, vertically"""
+        # Use: board.check_vertically()
+        # Before: same as check_horizontal
+        # After: same as checkhriz
         removed = False
         for i in range(9):
             for j in range(9):
                 if self.data[i][j][1] == True:
                     m,k = i%3,j%3
-                    if removed:
-                        dontcare = self.removefromrow(self.data[i][j][0],m,k)
-                    else:
-                        removed = self.removefromrow(self.data[i][j][0],m,k)
+                    removed |= self.remove_from_row(self.data[i][j][0],m,k)
         return removed
 
-    def removefromrow(self,number,m,k):
+    def remove_from_row(self,number,m,k):
         """removes a number from a row in the board"""
+        # Use: board.remove_from_row(number,m,k)
+        # Before: same as remove from line
+        # After: -||-
         removed = False
         for i in range(0,9,3):
             for j in range(0,9,3):
@@ -96,8 +110,11 @@ class Board:
                         removed = True
         return removed
 
-    def checkbox(self):
+    def check_box(self):
         """checks what possibilities are not ok, inside a box"""
+        # Use: board.check_box()
+        # Before: -||-
+        # After: -||-
         removed = False
         for i in range(9):
             for j in range(9):
@@ -110,58 +127,48 @@ class Board:
                                 removed = True
         return removed
 
-    def removeoptions(self):
+    def remove_options(self):
         """calls for the check* operations"""
-        print("removing possibilities from squares")
-        removed = self.checkhoriz()
-        print("    horizontally")
+        # Use: board.remove_options()
+        # Before: board: Board
+        # After: all the check operations have been executed
+        removed = self.check_horizontal()
         if removed:
-            self.maketrue()
-            dontcare = self.checkvertic()
-            print("    vertically")
+            self.make_true()
+            dontcare = self.check_vertically()
         else:
-            removed = self.checkvertic()
-            print("    vertically")
+            removed = self.check_vertically()
         if removed:
-            self.maketrue()
-            dontcare = self.checkbox()
-            print("    inside boxes")
+            self.make_true()
+            dontcare = self.check_box()
         else:
-            removed = self.checkbox()
-            print("    inside boxes")
+            removed = self.check_box()
         return removed
 
-    def whatsleft(self):
+    def what_is_left(self):
         """finds out what numbers are left and adds them as solutions"""
-        print("checking for single possibilites")
+        # Use: left = board.what_is_left()
+        # Before: board: Board
+        # After: what_is_left* operations have been executed
         removed = False
-        print("    horizontally")                                                    #Þarf þetta?
         for i in range(3):
             for j in range(3):
                 m,k = i//3,j//3
-                if removed:
-                    dontcare = self.deduceline(self.whatsleftinline(m,k),m,k)
-                else:
-                    removed = self.deduceline(self.whatsleftinline(m,k),m,k)
-        print("    vertically")                                                  #Þarf þetta?
-        for i in range(0,9,3):
+                removed |= self.deduce_line(self.what_is_left_in_line(m,k),m,k)
+        for i in range(0,9,3):                                              
              for j in range(0,9,3):
                  m,k = i%3,j%3
-                 if removed:
-                     dontcare = self.deducerow(self.whatsleftinrow(m,k),m,k)
-                 else:
-                     removed = self.deducerow(self.whatsleftinrow(m,k),m,k)                   
-        print("    inside boxes")
+                 removed |= self.deduce_row(self.what_is_left_in_row(m,k),m,k)                   
         for i in range(9):
-            if removed:
-                dontcare = self.deducebox(self.whatsleftinbox(i),i)
-            else:
-                removed = self.deducebox(self.whatsleftinbox(i),i)
+            removed |= self.deduce_box(self.what_is_left_in_box(i),i)
 
         return removed
 
-    def whatsleftinbox(self,i):
+    def what_is_left_in_box(self,i):
         """finds out what numbers are left to solve in a box"""
+        # Use: left = board.what_is_left_in_box(box)
+        # Before: board: Board, box: 0-8
+        # After: finds out what numbers are left to solve in a box
         left = [1,2,3,4,5,6,7,8,9]
         for j in range(9):
             if self.data[i][j][1] == True:
@@ -169,8 +176,12 @@ class Board:
                     
         return left
     
-    def deducebox(self,left,i):
+    def deduce_box(self,left,i):
         """finds what possibilites are the only ones left in a box and adds them"""
+        # Use: board.deduce_box(left,box)
+        # Before: board: Board, left: [], box: 0-8
+        # After: given what numbers are left to solve in a box, checks what numbers only
+        # appear once in a box and deduces that it must be a solution
         removed = False
         for number in left:
             often = 0
@@ -188,8 +199,11 @@ class Board:
                 removed = True
         return removed
 
-    def whatsleftinline(self,m,k):
+    def what_is_left_in_line(self,m,k):
         """finds out what numbers are left to solve in a line"""
+        # Use: left = board.what_is_left_in_line(m,k)
+        # Before: board: Board, m: 0-2, k: 0-2
+        # After: finds what numbers are left to solve in a line denoted by m and k
         left = [1,2,3,4,5,6,7,8,9]
         for i in range(3):
             for j in range(3):
@@ -197,8 +211,12 @@ class Board:
                       left.remove(self.data[i+m*3][j+3*k][0])
         return left
 
-    def deduceline(self,left,m,k):
+    def deduce_line(self,left,m,k):
         """finds what possibilites are the only ones left in a line and adds them"""
+        # Use: board.deduce_line(left,m,k)
+        # Before: board: Board, left: [], m: 0-2, k: 0-2
+        # After: given what numbers are left to solve in a line, checks what numbers only
+        # appear once in a line and deduces that it must be a solution
         removed = False
         for number in left:
             often = 0
@@ -217,8 +235,11 @@ class Board:
                 removed = True
         return removed
 
-    def whatsleftinrow(self,m,k):
+    def what_is_left_in_row(self,m,k):
         """finds out what numbers are left in the row"""
+        # Use: left = board.what_is_left_in_row(m,k)
+        # Before: board: Board, m: 0-2, k: 0-2
+        # After: same as in what_is_left_in_line
         left = [1,2,3,4,5,6,7,8,9]
         for i in range(3):
             for j in range(3):
@@ -226,8 +247,11 @@ class Board:
                       left.remove(self.data[i+m][j+k][0])
         return left
 
-    def deducerow(self,left,m,k):
+    def deduce_row(self,left,m,k):
         """finds what possibilites are the only ones left in a row and adds them"""
+        # Use: board.deduce_row(left,m,k)
+        # Before: board: Board, left: [], m: 0-2, k: 0-2
+        # After: same as in deduce_line
         removed = False
         for number in left:
             often = 0
@@ -246,22 +270,21 @@ class Board:
                 removed = True
         return removed
 
-    def trysolving(self):
+    def try_solving(self):
         """helping function"""
-        removed = self.removeoptions()
-        if removed:
-            dontcare = self.maketrue()
-        else:
-            removed = self.maketrue()
-        if removed:
-            print("some options were removed")
-            dontcare = self.whatsleft()
-        else:
-            removed = self.whatsleft()
+        # Use: board.try_solving()
+        # Before: board: Board
+        # After: calls for check* and what_is_left* operations
+        removed = self.remove_options()
+        removed |= self.make_true()
+        removed |= self.what_is_left()
         return removed
 
-    def maketrue(self):
+    def make_true(self):
         """makes a square true if there is single possibilites (solves)"""
+        # Use: board.make_true()
+        # Before: board: Board
+        # After: if a number was a single option but was False (not a solution) it is now True (a solution)
         madetrue = False
         for i in range(9):
             for j in range(9):
@@ -270,8 +293,11 @@ class Board:
                     madetrue = True
         return madetrue
 
-    def issolved(self):
+    def is_solved(self):
         """returns true if sudoku is solved, else false"""
+        # Use: is_board_solved = board.is_solved()
+        # Before: board: Board
+        # After: is_board_solved = True if the board is solved, False otherwise
         solved = True
         for i in range(9):
             for j in range(9):
@@ -280,20 +306,26 @@ class Board:
                     break
         return solved
 
-    def multiplesolutions(self):
+    def multiple_solutions(self):
         """returns multiple sudoku boards where a number has been picked"""
-        i,j = self.findlowestamount()
+        # Use: solutions = board.multiple_solutions()
+        # Before: board: Board
+        # After: solutions = [board1,board2,...]
+        i,j = self.find_lowest_amount()
         length = len(self.data[i][j])-1
         multipleboards = [None]*length
         for k in range(length):
             numbers = self.data[i][j][k]
             multipleboards[k] = copy.deepcopy(self.data)
             multipleboards[k][i][j] = [numbers,True]
-            print("Guessing on",numbers,"in",i,j)
+            #print("Guessing on",numbers,"in",i,j)
         return multipleboards
 
-    def findlowestamount(self):
+    def find_lowest_amount(self):
         """finds the lowest amount of options in a square on the whole board and returns the index of it"""
+        # Use: i,j = board.finelowestamount()
+        # Before: board: Board
+        # After: i,j = box, square where the lowest amount of options are
         counter = 3
         while counter <= 10:
             for i in range (9):
